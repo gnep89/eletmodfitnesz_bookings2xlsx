f52f690e1c9a93756047b98388af97f9267a549d
gnep <gnep89@gmail.com>
2025-04-15 13:25:14 +0200

    Variables to store button size added. Dates converted to GMT+2 in the output. date_to fixed, so bookings on the last day of the given timeframe would be included as well.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 
 # list of calendars in Booked4Us. The index corresponds to the ID.
 CALENDARS = ['Csoportos edzések', 'Kezelések']
+INPUT_BUTTON_SIZE = (10, 0)
+ACTION_BUTTON_SIZE = (8, 0)
 
 def get_last_month_start():
     today = dt.date.today()
@@ -28,18 +30,18 @@ def create_window():
     last_month_end = get_last_month_end()
     label_from = sg.Text('Ettől a dátumtól', size=17)
     input_from = sg.Input(key='date_from', default_text=last_month_start.strftime("%Y-%m-%d"))
-    button_from = sg.CalendarButton('Naptár', 'date_from', default_date_m_d_y=(last_month_start.month, last_month_start.day, last_month_start.year), format='%Y-%m-%d', size=(10, 0))
+    button_from = sg.CalendarButton('Naptár', 'date_from', default_date_m_d_y=(last_month_start.month, last_month_start.day, last_month_start.year), format='%Y-%m-%d', size=INPUT_BUTTON_SIZE)
 
     label_to = sg.Text('Eddig a dátumig', size=17)
     input_to = sg.Input(key='date_to', default_text=last_month_end.strftime("%Y-%m-%d"))
-    button_to = sg.CalendarButton('Naptár', 'date_to', default_date_m_d_y=(last_month_end.month, last_month_end.day, last_month_end.year), format='%Y-%m-%d', size=(10, 0))
+    button_to = sg.CalendarButton('Naptár', 'date_to', default_date_m_d_y=(last_month_end.month, last_month_end.day, last_month_end.year), format='%Y-%m-%d', size=INPUT_BUTTON_SIZE)
 
     label_export_dest = sg.Text('Excel file helye', size=17)
     input_export_dest = sg.Input(key='excel_dest')
-    button_export_dest = sg.FolderBrowse('Kiválaszt', key="export_dest", size=(10, 0))
+    button_export_dest = sg.FolderBrowse('Kiválaszt', key="export_dest", size=INPUT_BUTTON_SIZE)
 
-    button_get = sg.Button("Lekérdez", key='query', size=8)
-    button_exit = sg.Button("Kilép", key='exit', size=8)
+    button_get = sg.Button("Lekérdez", key='query', size=ACTION_BUTTON_SIZE)
+    button_exit = sg.Button("Kilép", key='exit', size=ACTION_BUTTON_SIZE)
 
     title = 'Foglalások lekérdezése'
     main_window = sg.Window(title,
@@ -59,8 +61,9 @@ def response_to_xlsx(reservations_response, filepath):
     worksheet.write_row(0, 0, ['Időpont', 'Típus', 'Vendég', 'Vendégek száma'], cell_format=workbook.add_format({'bold': True}))
 
     for idx, booking in enumerate(reservations_response):
-        # convert date from ISO to something more pleasant to the eye
-        start_time = dt.datetime.strptime(booking['StartTime'], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M")
+        # convert the UTC date from ISO to something more pleasant to the eye
+        gmt2 = dt.timezone(dt.timedelta(hours=2))
+        start_time = dt.datetime.strptime(booking['StartTime'], "%Y-%m-%dT%H:%M:%S.%fZ").astimezone(gmt2).strftime("%Y-%m-%d %H:%M")
         worksheet.write_row(idx+1, 0, [start_time, booking['Title'], booking['User']['Name']])
         worksheet.write_formula(idx+1, 3, f'COUNTIF(A:A, A{idx+2})')
     worksheet.autofilter(f'A1:C{len(reservations_response) + 1}')
@@ -69,8 +72,8 @@ def response_to_xlsx(reservations_response, filepath):
 
 def create_popup(filepath):
     popup_title = 'Az Excel file elkészült'
-    button_open = sg.Button("Megnyit", key='open', size=8)
-    button_close_popup = sg.Button("Bezár", key='close', size=8)
+    button_open = sg.Button("Megnyit", key='open', size=ACTION_BUTTON_SIZE)
+    button_close_popup = sg.Button("Bezár", key='close', size=ACTION_BUTTON_SIZE)
 
     popup_event, _ = sg.Window(popup_title,
                                layout=[[sg.T(filepath)],
@@ -92,7 +95,7 @@ while True:
     if event == 'query':
         # the request expects the ISO format
         date_from = values['date_from'] + 'T00:00:00.000Z'
-        date_to = values['date_to'] + 'T00:00:00.000Z'
+        date_to = values['date_to'] + 'T23:59:59.000Z'
 
         # create request
         calendar_id = CALENDARS.index(values['calendar']) + 1
